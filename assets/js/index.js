aa3a5ff13c77ca4ed4747bc5a9192e79c223df20
Natalia Balashova <88karamelka88@mail.ru>
2023-04-26 23:56:15 +0300

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
 
-fetch('https://swapi.dev/api/')
-.then(response => response.json())
-.then(json =>
-    console.log(json.people))
+const select = document.querySelector('.choose_select');
+const number = document.querySelector('#number');
+const btn = document.getElementById('btn');
+const text = document.querySelector('.text');
+let obj = [];
+select.addEventListener('change', chooseInfo);
+number.addEventListener('input', chooseInfo);
+
+function chooseInfo(){
+    let num = number.value;
+    fetch(`https://swapi.dev/api/${select.value}/${num}`)
+    .then(response => response.json())
+    .then(json =>
+        console.log(json)
+    )}
