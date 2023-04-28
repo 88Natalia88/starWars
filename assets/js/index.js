90fd02fd790746cdd765c0a5ef308a364483885f
Natalia Balashova <88karamelka88@mail.ru>
2023-04-28 10:00:27 +0300

    loading

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,34 +24,35 @@ function chooseInfo(){
         Gravity: ${json.gravity} <br> Created: ${json.created} <br>
         Edited: ${json.edited} <br> Population: ${json.population} <br> Terrain: ${json.terrain} <br>
         Surface_water: ${json.surface_water}`;
-    }else if(option[2].selected && num <= 10){
+    } else if(option[2].selected && num <= 10){
         text.innerHTML = `Title: ${json.title} <br> Director: ${json.director} <br> Episode_id: ${json.episode_id}
         Opening_crawl: ${json.opening_crawl} <br> Producer: ${json.producer} <br>
         Release_date: ${json.release_date} <br> Created: ${json.created} <br>
         Edited: ${json.edited}`;
-    }else if(option[3].selected && num <=10){
+    } else if(option[3].selected && num <=10){
         text.innerHTML = `Name: ${json.name} <br> Classification: ${json.classification} <br> Designation: ${json.designation}
         Hair_colors: ${json.hair_colors} <br> Skin_colors: ${json.skin_colors} <br>
         Average_height: ${json.average_height} <br> Created: ${json.created} <br>
         Edited: ${json.edited} <br> Eye_colors: ${json.eye_colors} <br> Language: ${json.language} <br>
         Average_lifespan: ${json.average_lifespan}`;
-    }else if(option[4].selected && num <=10){
+    } else if(option[4].selected && num <=10){
         text.innerHTML = `Name: ${json.name} <br> MGLT: ${json.MGLT} <br> Cargo_capacity: ${json.cargo_capacity}
         Consumables: ${json.consumables} <br> Cost_in_credits: ${json.cost_in_credits} <br>
         Crew: ${json.crew} <br> Created: ${json.created} <br>
         Edited: ${json.edited} <br> Lenght: ${json.lenght} <br> Manufacturer: ${json.manufacturer} <br>
         Hyperdrive_rating: ${json.hyperdrive_rating} <br> Max_atmosphering_speed: ${json.max_atmosphering_speed} <br> 
         Model: ${json.model} <br> Passengers: ${json.passengers} <br> StarShip_class: ${json.starship_class}`;
-    }else if(option[5].selected && num <= 10){
+    } else if(option[5].selected && num <= 10){
         text.innerHTML = `Name: ${json.name} <br> Cargo_capacity: ${json.cargo_capacity}
         Consumables: ${json.consumables} <br> Cost_in_credits: ${json.cost_in_credits} <br>
         Crew: ${json.crew} <br> Created: ${json.created} <br>
         Edited: ${json.edited} <br> Lenght: ${json.lenght} <br> Manufacturer: ${json.manufacturer} <br>
         Max_atmosphering_speed: ${json.max_atmosphering_speed} <br> 
         Model: ${json.model} <br> Passengers: ${json.passengers} <br> Vehicle_class: ${json.vehicle_class}`;
-    }else{
+    } else{
         text.innerHTML = `Выберите другую информацию, в числовом диапазоне от 1 до 10. Спасибо!`;
     }
+    document.querySelector('.loader').style.display = "none";
         //console.log(json)
     })
     .catch(error => text.innerHTML = `Произошла ошибка ${error.message}`)
